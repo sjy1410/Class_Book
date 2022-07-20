86adc0cdcda5708e5d292fe7aed2f604e2284ca5
songjuyoung-1410 <chicken010208@gmail.com>
2022-07-20 17:29:18 +0900

    css 수정

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Click from './component/Click.js';
+import Header from './component/Header.js';
+import './App.css';
+
+
+function App() {
+    return (
+      <div className='App'>
+        <Header />
+        <Click/>
+      </div>
+    );
+  };
+
+export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,13 @@
+import '../styleComponent/header.css'
+
+function Header(){
+    return(
+        <div className="header">
+            <header>
+                <h1>학교 교실 예약</h1>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
